a9e9ce435c960c16ec3ad84a68df46d7b8c5275b
rafael martins <rafael.martins87@outlook.com>
2021-07-27 16:34:55 -0300

    done contreroller

--- Controllers/TipoAvaliacaoController.cs
+++ Controllers/TipoAvaliacaoController.cs
@@ -1,11 +1,113 @@
-﻿using System;
+﻿using CadastrosGerais.Common;
+using CadastrosGerais.Models;
+using CadastrosGerais.Repository;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace CadastrosGerais.Controllers
 {
-    public class TipoAvaliacaoController
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("CorsAllowAll")]
+    public class TipoAvaliacaoController : ControllerBase
     {
+        private readonly ITipoAvaliacaoRepository _tipoAvaliacaoRepository;
+
+        public TipoAvaliacaoController(ITipoAvaliacaoRepository TipoAvaliacaoRepository)
+        {
+            _tipoAvaliacaoRepository = TipoAvaliacaoRepository;
+        }
+
+        /// <summary>
+        /// Insere um objeto para cadastro.
+        /// </summary>
+        /// <returns>Insere Cadastro</returns>
+        /// <response code="200">Cadastro inserido</response>
+        /// <response code="511">Usuário não autenticado</response>
+        /// <response code="500">Erro no método</response>
+        [HttpPost("Cria")]
+        public IActionResult TipoAvaliacaoAdd(TipoAvaliacaoModel dados)
+        {
+            try
+            {
+                return Ok(_tipoAvaliacaoRepository.TipoAvaliacaoAdd(dados));
+            }
+            catch (Exception ex)
+            {
+                new MyLog().GerarLog("TipoAvaliacaoAdd", "Erro ao buscar os TipoAvaliacaos", ex);
+                return BadRequest(new Error(HttpStatusCode.InternalServerError, "TipoAvaliacaoAdd", ex.Message));
+            }
+        }
+
+        /// <summary>
+        /// Get todos.
+        /// </summary>
+        /// <returns>Exibe todos os cadastros</returns>
+        /// <response code="200">Ok</response>
+        /// <response code="511">Usuário não autenticado</response>
+        /// <response code="500">Erro no método</response>
+        /// 
+        [HttpGet("GetAll")]
+        public IActionResult TipoAvaliacaoGetAll()
+        {
+            try
+            {
+                return Ok(_tipoAvaliacaoRepository.TipoAvaliacaoGetAll());
+            }
+            catch (Exception ex)
+            {
+                new MyLog().GerarLog("CadastroGet", "Erro ao buscar todas as avarias", ex);
+                return BadRequest(new Error(HttpStatusCode.InternalServerError, "CadastroGet", ex.Message));
+            }
+        }
+
+        /// <summary>
+        /// Atualiza dados.
+        /// </summary>
+        /// <returns>Insere Cadastro</returns>
+        /// <response code="200">Cadastro inserido</response>
+        /// <response code="511">Usuário não autenticado</response>
+        /// <response code="500">Erro no método</response>
+        /// 
+        [HttpPost("Update")]
+        public IActionResult TipoAvaliacaoUpd(TipoAvaliacaoModel dados)
+        {
+            try
+            {
+                return Ok(_tipoAvaliacaoRepository.TipoAvaliacaoUpd(dados));
+            }
+            catch (Exception ex)
+            {
+                new MyLog().GerarLog("CadastroUpd", "Erro ao buscar todas as avarias", ex);
+                return BadRequest(new Error(HttpStatusCode.InternalServerError, "CadastroUpd", ex.Message));
+            }
+        }
+
+        /// <summary>
+        /// Deletar dados.
+        /// </summary>
+        /// <returns>Insere Cadastro</returns>
+        /// <response code="200">Cadastro inserido</response>
+        /// <response code="511">Usuário não autenticado</response>
+        /// <response code="500">Erro no método</response>
+        /// 
+        [HttpDelete("Deleta")]
+        public IActionResult TipoAvaliacaoDel(int id)
+        {
+            try
+            {
+                return Ok(_tipoAvaliacaoRepository.TipoAvaliacaoDel(id));
+            }
+            catch (Exception ex)
+            {
+                new MyLog().GerarLog("CadastroDel", "Erro ao buscar todas as avarias", ex);
+                return BadRequest(new Error(HttpStatusCode.InternalServerError, "CadastroDel", ex.Message));
+            }
+        }
     }
 }
--- Controllers/TipoEstabelecimentoController.cs
+++ Controllers/TipoEstabelecimentoController.cs
@@ -1,11 +1,113 @@
-﻿using System;
+﻿using CadastrosGerais.Common;
+using CadastrosGerais.Models;
+using CadastrosGerais.Repository;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace CadastrosGerais.Controllers
 {
-    public class TipoEstabelecimentoController
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("CorsAllowAll")]
+    public class TipoEstabelecimentoController : ControllerBase
     {
+        private readonly ITipoEstabelecimentoRepository _tipoEstabelecimentoRepository;
+
+        public TipoEstabelecimentoController(ITipoEstabelecimentoRepository TipoEstabelecimentoRepository)
+        {
+            _tipoEstabelecimentoRepository = TipoEstabelecimentoRepository;
+        }
+
+        /// <summary>
+        /// Insere um objeto para cadastro.
+        /// </summary>
+        /// <returns>Insere Cadastro</returns>
+        /// <response code="200">Cadastro inserido</response>
+        /// <response code="511">Usuário não autenticado</response>
+        /// <response code="500">Erro no método</response>
+        [HttpPost("Cria")]
+        public IActionResult TipoEstabelecimentoAdd(TipoEstabelecimentoModel dados)
+        {
+            try
+            {
+                return Ok(_tipoEstabelecimentoRepository.TipoEstabelecimentoAdd(dados));
+            }
+            catch (Exception ex)
+            {
+                new MyLog().GerarLog("TipoEstabelecimentoAdd", "Erro ao buscar os TipoEstabelecimentos", ex);
+                return BadRequest(new Error(HttpStatusCode.InternalServerError, "TipoEstabelecimentoAdd", ex.Message));
+            }
+        }
+
+        /// <summary>
+        /// Get todos.
+        /// </summary>
+        /// <returns>Exibe todos os cadastros</returns>
+        /// <response code="200">Ok</response>
+        /// <response code="511">Usuário não autenticado</response>
+        /// <response code="500">Erro no método</response>
+        /// 
+        [HttpGet("GetAll")]
+        public IActionResult TipoEstabelecimentoGetAll()
+        {
+            try
+            {
+                return Ok(_tipoEstabelecimentoRepository.TipoEstabelecimentoGetAll());
+            }
+            catch (Exception ex)
+            {
+                new MyLog().GerarLog("CadastroGet", "Erro ao buscar todas as avarias", ex);
+                return BadRequest(new Error(HttpStatusCode.InternalServerError, "CadastroGet", ex.Message));
+            }
+        }
+
+        /// <summary>
+        /// Atualiza dados.
+        /// </summary>
+        /// <returns>Insere Cadastro</returns>
+        /// <response code="200">Cadastro inserido</response>
+        /// <response code="511">Usuário não autenticado</response>
+        /// <response code="500">Erro no método</response>
+        /// 
+        [HttpPost("Update")]
+        public IActionResult TipoEstabelecimentoUpd(TipoEstabelecimentoModel dados)
+        {
+            try
+            {
+                return Ok(_tipoEstabelecimentoRepository.TipoEstabelecimentoUpd(dados));
+            }
+            catch (Exception ex)
+            {
+                new MyLog().GerarLog("CadastroUpd", "Erro ao buscar todas as avarias", ex);
+                return BadRequest(new Error(HttpStatusCode.InternalServerError, "CadastroUpd", ex.Message));
+            }
+        }
+
+        /// <summary>
+        /// Deletar dados.
+        /// </summary>
+        /// <returns>Insere Cadastro</returns>
+        /// <response code="200">Cadastro inserido</response>
+        /// <response code="511">Usuário não autenticado</response>
+        /// <response code="500">Erro no método</response>
+        /// 
+        [HttpDelete("Deleta")]
+        public IActionResult TipoEstabelecimentoDel(int id)
+        {
+            try
+            {
+                return Ok(_tipoEstabelecimentoRepository.TipoEstabelecimentoDel(id));
+            }
+            catch (Exception ex)
+            {
+                new MyLog().GerarLog("CadastroDel", "Erro ao buscar todas as avarias", ex);
+                return BadRequest(new Error(HttpStatusCode.InternalServerError, "CadastroDel", ex.Message));
+            }
+        }
     }
 }
